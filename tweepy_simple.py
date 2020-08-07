fd99ac690446b9633e3f9b1b9b0d67a47f4c8275
Kieran Schubert <schubert.kieran@gmail.com>
2020-08-07 13:11:41 +0200

    collection+processing of tweets OK

--- tweepy_simple.py
+++ tweepy_simple.py
@@ -1,13 +1,65 @@
 import tweepy
 import twitter_credentials
+import pandas as pd
+import numpy as np
+import re
+from textblob import TextBlob
+
+
+class TweetCollector():
+	"""
+	Class for collecting and processing tweets.
+	"""
+
+	def clean_tweet(self, tweet):
+		return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+	def collect_tweets(self, hashtag, max_tweets):
+		tweets = []
+		for tweet in tweepy.Cursor(api.search, q=hashtag, lang='en').items(MAX_TWEETS):
+			tweets.append(tweet)
+
+		return tweets
+
+	def tweets_to_data_frame(self, tweets):
+		df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['tweets'])
+
+		df['id'] = np.array([tweet.id for tweet in tweets])
+		df['len'] = np.array([len(tweet.text) for tweet in tweets])
+		df['date'] = np.array([tweet.created_at for tweet in tweets])
+		df['source'] = np.array([tweet.source for tweet in tweets])
+		df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
+		df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
+
+		return df
+
+	def analyze_sentiment(self, tweet):
+		analysis = TextBlob(tweet)
+		
+		if analysis.sentiment.polarity > 0:
+			return 1
+		elif analysis.sentiment.polarity == 0:
+			return 0
+		else:
+			return -1
+
 
 MAX_TWEETS = 100
 
 auth = tweepy.OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
 api = tweepy.API(auth)
 
-for tweet in tweepy.Cursor(api.search, q='#AAPL', rpp=100, lang='en').items(MAX_TWEETS):
+tweet_collector = TweetCollector()
 
-    print(tweet.text)
-    print("-"*10)
-    
+tweets = tweet_collector.collect_tweets(hashtag="#AAPL", max_tweets=MAX_TWEETS)
+tweets_df = tweet_collector.tweets_to_data_frame(tweets)
+tweets_df['tweets'] = pd.DataFrame([tweet_collector.clean_tweet(tweet) for tweet in tweets_df['tweets']])
+tweets_df['sentiment'] = np.array([tweet_collector.analyze_sentiment(tweet) for tweet in tweets_df['tweets']])
+
+#[print(tweet, sent) for tweet, sent in zip(tweets_df['tweets'][:30], tweets_df['sentiment'][:30])]
+
+
+
+
+
+
